ce20aa6f2cb816c1d31c783f8295d8b40785a5be
zahidyew <zahidyew96@gmail.com>
2021-02-18 03:23:41 +0800

    show Toast after user set default location

--- src/components/SetDefaultBtn.js
+++ src/components/SetDefaultBtn.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ToastContainer, toast, Flip } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SetDefaultBtn = ({setDefault}) => {
    const handleClick = () => {
-      //console.log('clicked');
       setDefault();
+      showToast();
    }
+   
+   const showToast = () =>
+      toast.dark("Default location saved.", {
+         position: "bottom-center",
+         autoClose: 3000,
+         hideProgressBar: true,
+         closeOnClick: true,
+         pauseOnHover: false,
+         draggable: true,
+         progress: undefined,
+      });
 
    return (
-      <button onClick={handleClick}> Set as default location </button>
+      <> 
+         <button onClick={handleClick}> Set as default </button>
+         <ToastContainer limit={1} transition={Flip}/>
+      </>
    )
 }
 
